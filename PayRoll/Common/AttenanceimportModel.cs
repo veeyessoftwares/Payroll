5cc6b0f73576f88096baf232b199fa9990997751
Rajkumar <rajkumarbecse59@gmail.com>
2017-08-29 01:25:06 +0530

    added missed files

---
+++ PayRoll/Common/AttenanceimportModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class AttenanceimportModel
+    {
+        public int Id { get; set; }
+        public int AttendanceLogId { get; set; }
+        public Nullable<System.DateTime> AttendanceDate { get; set; }
+        public Nullable<int> EmployeeId { get; set; }
+        public Nullable<System.TimeSpan> InTime { get; set; }
+        public Nullable<System.TimeSpan> OutTime { get; set; }
+        public string PunchRecords { get; set; }
+        public decimal ShiftCount { get; set; }
+    }
+}
---
+++ PayRoll/DAL/Table/ShiftMaster.cs
@@ -0,0 +1,17 @@
+﻿namespace DAL
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    [Table("ShiftMaster")]
+    public partial class ShiftMaster
+    {
+        public int ID { get; set; }
+        public int WGID { get; set; }
+        public TimeSpan InTime { get;set; }
+        public TimeSpan OutTime { get; set; }
+        public TimeSpan Hours { get; set; }
+    }
+}
